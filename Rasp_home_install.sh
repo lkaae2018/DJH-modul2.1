bb669f3557efab02e8c315c1bed73e977817ea4f
lkaae2018 <lak@djhhadsten.dk>
2020-04-13 07:58:32 +0200

    Home

---
+++ Rasp_home_install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python-dev
+sudo apt-get install python-pip
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+cd Adafruit_Python_DHT
+sudo python setup.py install
+sudo pip install pubnub
+
